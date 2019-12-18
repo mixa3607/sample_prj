e49ed041bbd4d22dae66594c13ae2a8b11483d93
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2019-12-19 03:42:43 +0500

    optimize htaccess

--- Library/index.php
+++ Library/index.php
@@ -4,18 +4,10 @@ spl_autoload_register(function ($class) {
 });
 
 list($path, $qs) = explode("?", $_SERVER["REQUEST_URI"], 2);
-//parse_str($qs, $args);
 $path_nodes = explode("/", $path);
-
-//if ($path_nodes[1] != "api"){
-//    echo json_encode(Array('error' => 'routing error'));
-//    http_response_code(500);
-//    return;
-//}
 
 switch ($path_nodes[1]){
     case '':
-    case 'library':
         \Controller\Library::ComposePage();
         break;
     default:
