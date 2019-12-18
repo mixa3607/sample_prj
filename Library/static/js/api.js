9f71fcc51b901cb2189459287baffcc771ed5248
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2019-12-18 21:50:51 +0500

    partially implement js api

---
+++ Library/static/js/api.js
@@ -0,0 +1,68 @@
+class Api {
+    static async getBooks() {
+        let booksReq = await fetch("/api/books", {method: 'GET'});
+        let booksJson = await booksReq.json();
+        return booksJson;
+    }
+
+    static async deleteBook(bookId) {
+        let booksReq = await fetch("/api/book?id=" + bookId, {method: 'DELETE'});
+        let booksJson = await booksReq.json();
+        return booksJson;
+    }
+
+    static async addBook(book){
+        let url = `/api/book?title=${book.title}&pub_date=${book.pubDate}`;
+        book.authors.forEach(author => url += "&authors[]=" + author);
+        book.genres.forEach(genre => url += "&genres[]=" + genre);
+        let resp = await fetch(url, {method: 'POST'});
+        //if (resp.code === 200){
+            return await resp.json();
+        //}
+        //else{
+        //    throw new Error("Request error");
+        //}
+    }
+
+    static async changeBookTitle(bookId, title) {
+        let req = await fetch(`/api/book?id=${bookId}$title=${title}`, {method: 'PUT'});
+        let respJson = await req.json();
+        return respJson;
+    }
+
+    static async changeBookPubDate(bookId, pubDate) {
+        let req = await fetch(`/api/book?id=${bookId}$pub_date=${pubDate}`, {method: 'PUT'});
+        let respJson = await req.json();
+        return respJson;
+    }
+
+    static async changeBookAuthors(bookId, authors) {
+        let url = `/api/book?id=${bookId}`;
+        authors.forEach(author => url += "&authors[]=" + author);
+
+        let req = await fetch(url, {method: 'PUT'});
+        let respJson = await req.json();
+        return respJson;
+    }
+
+    static async changeBookGenres(bookId, genres) {
+        let url = `/api/book?id=${bookId}`;
+        genres.forEach(genre => url += "&genres[]=" + genre);
+
+        let req = await fetch(url, {method: 'PUT'});
+        let respJson = await req.json();
+        return respJson;
+    }
+
+    static async getAuthors() {
+        let req = await fetch("/api/authors", {method: 'GET'});
+        let respJson = await req.json();
+        return respJson;
+    }
+
+    static async getGenres() {
+        let req = await fetch("/api/genres", {method: 'GET'});
+        let respJson = await req.json();
+        return respJson;
+    }
+}
